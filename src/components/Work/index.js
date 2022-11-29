92d74e495997892777d4b18b5493b9bf187580dc
Duport <seishy93@gmail.com>
2022-11-29 11:11:38 +0100

    refacto data of work component

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -6,6 +6,7 @@ import meteo from '../../assets/ApiMeteo.jpg';
 import pfc from '../../assets/PFC.jpg';
 import portfolio from '../../assets/portfolio.jpg';
 
+import { data } from '../../data';
 import './work.scss';
 
 function Work () {
@@ -24,76 +25,27 @@ function Work () {
         <span>t</span>
         <span>s</span>
       </h1>
-        <article className="work_article">
+        {data.map((item) => (
+        <article 
+          className="work_article"
+          key={item.title}
+        >
           <h2 className="work_article-title">
-            Match-Job
+            {item.title}
           </h2>
-          <img src={matchJob1} className="work_article-image" />
+          <img src={item.image} className="work_article-image" />
           <ul className="work_article-techno">
-            <li>#CSS3</li>
-            <li>#JavaScript</li>
-            <li>#React</li>
-            <li>#ReactRouter</li>
-            <li>#Axios</li>
-            <li>#Symfony</li>
+            {item.technos.map((items) => (
+              <li key={items}>{items}</li>
+            ))}
           </ul>
           <p className="work_article-description">
-            Match-Job est une application de recherche d'emploi pour les développeurs web. Elle a été réalisé en 1 mois, dans le cadre d'un projet de fin de formation.
+            {item.description}
           </p>
-          <a href='https://youtu.be/WjrPnFDAWpw?t=487' className="work_article-link" target="_blank">Voir la présentation</a>
-          <a href='https://github.com/StevenDuport/Match-Job' className="work_article-link" target="_blank">GitHub</a>
+          {item.link && <a href={item.link} className="work_article-link" target="_blank">Découvrir le site</a>}
+          <a href={item.gitHub} className="work_article-link" target="_blank">GitHub</a>
         </article>
-
-        <article className="work_article2">
-          <h2 className="work_article-title">
-            Météo-API
-          </h2>
-          <img src={meteo} className="work_article-image" />
-          <ul className="work_article-techno">
-            <li>#HTML5</li>
-            <li>#CSS3</li>
-            <li>#JavaScript</li>
-          </ul>
-          <p className="work_article-description">
-            Ce site permet de récupérer la météo d'une ville grâce à une barre de recherche. Réalisé en parallèle de ma formation chez O'clock, il avait pour but de m'entrainer à utiliser une API.
-          </p>
-          <a href="http://meteo-api.surge.sh" className="work_article-link" target="_blank">Visiter le site</a>
-          <a href='https://github.com/StevenDuport/Meteo-API' className="work_article-link" target="_blank">GitHub</a>
-        </article>
-
-        <article className="work_article">
-          <h2 className="work_article-title">
-            Pierre Feuille Ciseaux
-          </h2>
-          <img src={pfc} className="work_article-image" />
-          <ul className="work_article-techno">
-            <li>#HTML5</li>
-            <li>#CSS3</li>
-            <li>#JavaScript</li>
-          </ul>
-          <p className="work_article-description">
-          Réalisation du "célèbre" jeu: Pierre Feuille Ciseaux.
-          </p>
-          <a href='https://pierrepapierciseaux.surge.sh/' className="work_article-link" target="_blank">Visiter le site</a>
-          <a href='https://github.com/StevenDuport/Pierre-Feuille-Ciseaux' className="work_article-link" target="_blank">GitHub</a>
-        </article>
-
-        <article className="work_article">
-          <h2 className="work_article-title">
-            Portfolio
-          </h2>
-          <img src={portfolio} className="work_article-image" />
-          <ul className="work_article-techno">
-            <li>#CSS3</li>
-            <li>#JavaScript</li>
-            <li>#React</li>
-            <li>#ReactRouter</li>
-          </ul>
-          <p className="work_article-description">
-            Réalisation de ce portfolio.
-          </p>
-          <a href='https://github.com/StevenDuport/Pierre-Feuille-Ciseaux' className="work_article-link" target="_blank">GitHub</a>
-        </article>
+      ))}
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,25 +1,54 @@
-import matchJob1 from '../src/assets/matchJob1.jpg';
 
 export const data = [
   {
     title : "Match-Job",
-    description : "Match-Job est une application de recherche d'emploi pour les développeurs web. Elle a été réalisé en 1 mois, dans le cadre d'un projet de fin de formation.",
-    picture: {matchJob1},
-    technos: "ReactJS, Redux, Symfony",
-    link: "",
+    description : " Match-Job est une application de recherche d'emploi pour les développeurs web. Elle a été réalisé en 1 mois, dans le cadre d'un projet de fin de formation.",
+    technos: [
+      "#CSS3",
+      "#Javascript",
+      "#React",
+      "#ReactRouter",
+      "#Axios",
+      "#Symfony"
+    ],
+    link: "https://youtu.be/WjrPnFDAWpw?t=487",
+    gitHub: "https://github.com/StevenDuport/Match-Job",
+    image: require('../src/assets/matchJob1.jpg')
   },
   {
     title : "Météo-API",
-    description : "Ce site permet de récupérer la météo d'une ville grâce à une barre de recherche. Réalisé en parallèle de ma formation chez O'clock, il avait pour but de m'entrainer à utiliser une API.",
-    picture: {matchJob1},
-    technos: "HTML5, CSS3, JavaScript",
-    link: "",
+    description : " Ce site permet de récupérer la météo d'une ville grâce à une barre de recherche. Réalisé en parallèle de ma formation chez O'clock, il avait pour but de m'entrainer à utiliser une API.",
+    technos: [
+      "#HTML5",
+      "#CSS3",
+      "#Javascript",
+    ],
+    link: "http://meteo-api.surge.sh",
+    gitHub: "https://github.com/StevenDuport/Meteo-API",
+    image: require('../src/assets/ApiMeteo.jpg')
   },
   {
     title : "Pierre Feuille Ciseaux",
-    description : "Réalisation du 'célèbre' jeu Pierre Feuille Ciseaux.",
-    picture: {matchJob1},
-    technos: "HTML5, CSS3, JavaScript",
-    link: "",
+    description :'Réalisation du "célèbre" jeu: Pierre Feuille Ciseaux.',
+    technos: [
+      "#HTML5",
+      "#CSS3",
+      "#Javascript",
+    ],
+    link: "https://pierrepapierciseaux.surge.sh/",
+    gitHub: "https://github.com/StevenDuport/Pierre-Feuille-Ciseaux",
+    image: require('../src/assets/PFC.jpg')
+  },
+  {
+    title : "Portfolio",
+    description : "Réalisation de ce portfolio.",
+    technos: [
+      "#CSS3",
+      "#Javascript",
+      "#React",
+      "#ReactRouter",
+    ],
+    gitHub: "https://github.com/StevenDuport/Portfolio",
+    image: require('../src/assets/portfolio.jpg')
   },
 ]
