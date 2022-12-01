1fdd9d29be572f9e9c815e761292544749408f2c
Duport <seishy93@gmail.com>
2022-12-01 16:52:39 +0100

    change for mobile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -71,23 +71,7 @@ function About() {
         <p className="about_text-item">Passioné par <span className="about_span">l'illustration</span> et <span className="about_span">l'art</span>, j'aime mettre ma créativité à profit dans mes projets.</p>
       </div>
       <div className="about_skills">
-        <svg viewBox="0 0 500 500">
-          <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-          <text width="500">
-            <textPath xlinkHref="#curve" startOffset="4%">
-              Technos utilisées
-            </textPath>
-          </text>
-        </svg>
-          <p className={topHead ? 'about_arrow-text--clicked' : 'about_arrow-text'}>Click </p>
-          <img src={arrow} className={topHead ? 'about_arrow--clicked' : 'about_arrow'}/>
-        <button 
-          className="about_button"
-          onClick={openHead}
-        >
-          <img src={headTop} className={topHead ? 'about_head-cliked' : 'about_head-top'}/>
-          <img src={head} className="about_head"/>
-        </button>
+          <p className="about_title-techno">Technos utilisées</p>
         <ul className="about_list">
           <li className={technos ? 'about_list-item-cliked' : 'about_list-item'}>HTML5</li>
           <li className={technos ? 'about_list-item-cliked' : 'about_list-item'}>CSS3</li>
@@ -97,6 +81,15 @@ function About() {
           <li className={technos ? 'about_list-item-cliked' : 'about_list-item'}>Git/GitHub</li>
           <li className={technos ? 'about_list-item-cliked' : 'about_list-item'}>Procreate</li>
         </ul>
+          <p className={topHead ? 'about_arrow-text--clicked' : 'about_arrow-text'}>Click </p>
+          <img src={arrow} className={topHead ? 'about_arrow--clicked' : 'about_arrow'}/>
+        <button 
+          className="about_button"
+          onClick={openHead}
+        >
+          <img src={headTop} className={topHead ? 'about_head-cliked' : 'about_head-top'}/>
+          <img src={head} className="about_head"/>
+        </button>
       </div>
     </div>
   );
